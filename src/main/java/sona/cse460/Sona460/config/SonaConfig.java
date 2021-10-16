98e07dd396814764feeebd980c0e2fb2aba0df94
Jacob Goldverg <jgoldverg@gmail.com>
2021-10-16 18:35:09 -0400

    connecting to postgresql

--- src/main/java/sona/cse460/Sona460/config/SonaConfig.java
+++ src/main/java/sona/cse460/Sona460/config/SonaConfig.java
@@ -1,10 +1,6 @@
 package sona.cse460.Sona460.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SonaConfig {
--- src/main/java/sona/cse460/Sona460/model/OneDayCovid.java
+++ src/main/java/sona/cse460/Sona460/model/OneDayCovid.java
@@ -1,19 +1,22 @@
 package sona.cse460.Sona460.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name="days")
+@Data
+@Table(name = "days")
 public class OneDayCovid {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Temporal(TemporalType.DATE)
     private Date date;
 
-    @Id
-    @GeneratedValue
-    private Long id;
 
 
 }
