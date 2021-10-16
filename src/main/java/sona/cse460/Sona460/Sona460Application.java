d57fca7b8e14ffad4caed7b6452c14f7f797b00b
Jacob Goldverg <jgoldverg@gmail.com>
2021-10-16 16:27:51 -0400

    initial spring repo

---
+++ src/main/java/sona/cse460/Sona460/Sona460Application.java
@@ -0,0 +1,13 @@
+package sona.cse460.Sona460;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sona460Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sona460Application.class, args);
+	}
+
+}
